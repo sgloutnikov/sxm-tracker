32d4975b4ff62be3b59aa876e3c5b633fb807889
Stefan Gloutnikov <stefan@gloutnikov.com>
2016-06-20 02:51:22 +0300

    Adding logging

--- sxm_collect.py
+++ sxm_collect.py
@@ -1,18 +1,25 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import time
+import logging.config
+
+logging.config.fileConfig('logging_config.ini')
+logger = logging.getLogger(__name__)
 
 
 def timed_job():
     print('Work Work!')
+    logger.info("Log Work")
     more_work()
 
 
 def more_work():
     time.sleep(3)
     print("More Work...")
+    logger.info("Log More Work")
 
 
 if __name__ == "__main__":
+    logger.info("Starting")
     scheduler = BlockingScheduler()
     scheduler.add_job(timed_job, 'interval', seconds=5)
     scheduler.start()
