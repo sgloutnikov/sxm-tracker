da92fac2318434b53a61e353ae34b20280ef59c8
Stefan Gloutnikov <stefan@gloutnikov.com>
2016-06-20 19:15:42 +0300

    Cleaning up and adding packages + sample api data

--- sxm_collect.py
+++ sxm_collect.py
@@ -1,18 +1,22 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import logging.config
+from api import api_manager
 
 logging.config.fileConfig('logging_config.ini')
 logger = logging.getLogger(__name__)
 
 
-def collect_trigger():
+def collect_now_playing():
     # TODO: Look into Redis Queue (http://python-rq.org)
     logger.info("Log Work Work!")
 
 
+def collect_full_api_data():
+    pass
+
 if __name__ == "__main__":
     # TODO: When in django, probably will have to move to BackgroundScheduler
     scheduler = BlockingScheduler()
-    scheduler.add_job(collect_trigger, 'interval', seconds=30)
+    scheduler.add_job(collect_now_playing, 'interval', seconds=30)
     scheduler.start()
 
