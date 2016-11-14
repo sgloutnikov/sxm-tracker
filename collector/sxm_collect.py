003f17da61983091554a4fe67e955014917305a6
Stefan Gloutnikov <stefan@gloutnikov.com>
2016-11-14 05:55:33 +0200

    Updating logging setup

--- collector/sxm_collect.py
+++ collector/sxm_collect.py
@@ -6,7 +6,7 @@ from collector.api import api_manager
 from collector.scrubber import scrub_manager
 
 
-log_config = fpath = os.path.join(os.path.dirname(__file__), 'logging_config.ini')
+log_config = os.path.join(os.path.dirname(__file__), 'logging_config.ini')
 logging.config.fileConfig(log_config, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
