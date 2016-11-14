c2a3962e3844a1ebae7058a97f927fb357343b88
Stefan Gloutnikov <stefan@gloutnikov.com>
2016-11-14 06:13:22 +0200

    Adding host

--- sxmtracker/settings.py
+++ sxmtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['theheattracker.herokuapp.com']
+ALLOWED_HOSTS = ['theheattracker.herokuapp.com', 'theheat.gloutnikov.com']
 
 
 # Application definition
