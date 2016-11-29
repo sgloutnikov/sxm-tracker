106137921b6fe0c94d099220954f6a68d9e0c831
Stefan Gloutnikov <stefan@gloutnikov.com>
2016-11-29 13:29:08 +0200

    Updating filters and tools after refactor

--- tools/db_backfill_scrub.py
+++ tools/db_backfill_scrub.py
@@ -12,14 +12,17 @@ import time
 
 logging.basicConfig(filename='db_backfil_scrub.log',level=logging.DEBUG)
 
+station = 'thehighway'
 MONGODB_URI = os.environ.get('MONGODB_URI')
 client = MongoClient(MONGODB_URI)
 db = client.get_default_database()
+collection_nowplaying = db[station + '_nowplaying']
+collection_songs = db[station + '_songs']
 
-scrub_manager.init()
+scrub_manager.init(station)
 
 #songs = db.nowplaying.find({'song': {"$regex": 'Break Up'}}).sort("startTime", direction=ASCENDING)
-songs = db.nowplaying.find({"song": "Nothing On You"}).sort("startTime", direction=ASCENDING)
+songs = collection_nowplaying.find({"song": "Think Of You"}).sort("startTime", direction=ASCENDING)
 
 counter = 1
 for song in songs:
@@ -27,14 +30,14 @@ for song in songs:
     print(str(counter))
     print(song)
     song_id = song.get('_id')
-    song = scrub_manager.scrub_artist(song)
+    song = scrub_manager.scrub_artist(station, song)
     print(song)
-    song = scrub_manager.scrub_song(song)
+    song = scrub_manager.scrub_song(station, song)
     print(song)
     song = api_manager.get_spotify(song)
     song.pop('_id', None)
     print(song)
-    db.nowplaying.replace_one({'_id': ObjectId(song_id)}, song)
-    db_manager.save_in_songs(song)
+    collection_nowplaying.replace_one({'_id': ObjectId(song_id)}, song)
+    db_manager.save_in_songs(station, song)
     counter += 1
     time.sleep(0.5)
--- tools/db_data_scrub.py
+++ tools/db_data_scrub.py
@@ -4,33 +4,35 @@ from db import db_manager
 import re
 import os
 
+station = 'thehighway'
+collection = db_manager.db[station + "_nowplaying"]
 total = 0
 print("--- Starting Manual DB Clean ---")
 
 # Artist
 print("--- Artist ---")
-fpath = os.path.join(os.path.dirname(__file__), '../filter_lists/bad_artist.txt')
+fpath = os.path.join(os.path.dirname(__file__), '../filter_lists/' + station + '/bad_artist.txt')
 with open(fpath, 'r') as f:
     for line in f:
         pattrn = str(line).strip()
         print("Checking: " + pattrn)
         REGEX = re.compile(pattrn, re.IGNORECASE)
         # Remove Here
-        result = db_manager.db.nowplaying.delete_many(({'artist': {'$regex': REGEX}}))
+        result = collection.delete_many(({'artist': {'$regex': REGEX}}))
         total += result.deleted_count
         print("Deleted: " + str(result.deleted_count))
 
 
 # Song
 print("--- Song ---")
-fpath = os.path.join(os.path.dirname(__file__), '../filter_lists/bad_song.txt')
+fpath = os.path.join(os.path.dirname(__file__), '../filter_lists/' + station + '/bad_song.txt')
 with open(fpath, 'r') as f:
     for line in f:
         pattrn = str(line).strip()
         print("Checking: " + pattrn)
         REGEX = re.compile(pattrn, re.IGNORECASE)
         # Remove Here
-        result = db_manager.db.nowplaying.delete_many(({'song': {'$regex': REGEX}}))
+        result = collection.delete_many(({'song': {'$regex': REGEX}}))
         total += result.deleted_count
         print("Deleted: " + str(result.deleted_count))
 
